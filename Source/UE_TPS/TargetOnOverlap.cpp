ad0685207a1a76446b7c75bee680b3ef5c5eee63
Kyscen <59921790+Kyscen@users.noreply.github.com>
2023-01-25 13:01:46 +0100

    BoxCollision (en cours)

---
+++ Source/UE_TPS/TargetOnOverlap.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TargetOnOverlap.h"
+
+// Sets default values for this component's properties
+UTargetOnOverlap::UTargetOnOverlap()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	UBoxComponent* HitBox = CreateDefaultSubobject<UBoxComponent>(TEXT("HitBox"));
+	// ...
+}
+
+
+// Called when the game starts
+void UTargetOnOverlap::BeginPlay()
+{
+	Super::BeginPlay();
+	HitBox->OnComponentBeginOverlap.AddDynamic(this, &UTargetOnOverlap::OnOverlapBegin);
+
+	// ...
+
+}
+
+
+// Called every frame
+void UTargetOnOverlap::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UTargetOnOverlap::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap Begin"));
+	}
---
+++ Source/UE_TPS/TargetOnOverlap.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/BoxComponent.h"
+#include "TargetOnOverlap.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UE_TPS_API UTargetOnOverlap : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTargetOnOverlap();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* HitBox;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
